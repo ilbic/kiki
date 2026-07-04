a996a655fae378d582b463227de4b2893fcac5d9
judahhooper <judahhooper@icloud.com>
2026-07-04 07:25:40 +0100

    setup class position and fen.py

---
+++ position.py
@@ -0,0 +1,25 @@
+class Position:
+
+    white_pawns = 0
+    white_knights = 0
+    white_bishops = 0
+    white_rooks = 0
+    white_queen = 0
+    white_king = 0
+
+    black_pawns = 0
+    black_knights = 0
+    black_bishops = 0
+    black_rooks = 0
+    black_queen = 0
+    black_king = 0
+
+    active_colour = white
+
+    castling_availability = KQkq
+
+    en_passant_target_square = -
+
+    halfmove_clock = 0
+
+    fullmove_number = 0
